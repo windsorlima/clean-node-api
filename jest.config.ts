8d45ca6646d15e1e208edf81f70b7fed001526da
Windsor Lima <windsorlimad@gmail.com>
2022-01-18 01:56:19 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+  roots: ['<rootDir>/src'],
+  collectCovarageFrom: ['<rootDir>/src/**/*ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
